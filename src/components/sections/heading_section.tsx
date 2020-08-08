f8e0fdba54323f11a4ba6d46fa25c0656921ec1e
Israel <dragon-zisrael@hotmail.com>
2020-08-07 23:42:10 -0500

    Added heading section

--- src/components/sections/heading_section.tsx
+++ src/components/sections/heading_section.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from '../../variables.scss';
 
 export default function HeadingSection() {
 	const data = useStaticQuery(graphql`
@@ -18,9 +20,33 @@ export default function HeadingSection() {
 		<div className='flex flex-row justify-around'>
 			<Img
 				fluid={{ ...data.file.childImageSharp.fluid, aspectRatio: 1 }}
-				style={{ width: '30vw' }}
+				style={{ width: '90vh' }}
 			/>
-			<div>text</div>
+			<div className='flex flex-col font-hairline tracking-wider uppercase my-16 pl-10'>
+				<div className='relative text-6xl' style={{ right: '150px' }}>
+					Great ideas
+				</div>
+				<div
+					className='c-primary relative overflow-visible whitespace-no-wrap text-6xl'
+					style={{ right: '150px' }}
+				>
+					Better future
+				</div>
+				<hr />
+				<div className='flex-grow' />
+				<div className='font-thin'>
+					Send an email. Request a meeting and get your dreams built
+					in no time
+				</div>
+				<div className='cursor-pointer c-primary font-normal'>
+					Start a project{' '}
+					<FontAwesomeIcon
+						icon='arrow-right'
+						color={styles.deimosPrimary}
+						style={{ color: styles.deimosPrimary }}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import HeadingSection from '../components/sections/heading_section';
 
 export default function index() {
 	return (
-		<div>
-			<Navbar />
+		<div className='px-16 py-5'>
+			{/* <Navbar /> */}
 			<HeadingSection />
 		</div>
 	);
